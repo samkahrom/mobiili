4788dbe855be17038d33ae255280425c87bbbf01
Sam Kahrom <sam.kahrom@myy.haaga-helia.fi>
2020-09-28 12:14:18 +0300

    Kartta

--- addresswithlocation/App.js
+++ addresswithlocation/App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: 60.201373,
+          longitude: 24.934041,
+        }}
+        title="Haaga-Helia"
+      />
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
